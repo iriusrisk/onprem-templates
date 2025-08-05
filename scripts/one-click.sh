03fab3d88177af38f7fe2c0bb057c8a78f0cf3b9
pwildber <pwildber@iriusrisk.com>
2025-08-05 11:52:57 +0100

    DP-720: missed ;;

--- scripts/one-click.sh
+++ scripts/one-click.sh
@@ -231,14 +231,15 @@ case "$CONTAINER_ENGINE" in
         # Bring up containers, which will join the existing pod
         eval "$UP_CMD"
 
-    # -- SYSTEMD autostart for the pod --
-    sudo podman generate systemd --name $POD_NAME --files --restart-policy=always
-    sudo mv pod-$POD_NAME.service /etc/systemd/system/
-    sudo systemctl daemon-reload
-    sudo systemctl enable pod-$POD_NAME.service
-    sudo systemctl start pod-$POD_NAME.service
+        # -- SYSTEMD autostart for the pod --
+        sudo podman generate systemd --name $POD_NAME --files --restart-policy=always
+        sudo mv pod-$POD_NAME.service /etc/systemd/system/
+        sudo systemctl daemon-reload
+        sudo systemctl enable pod-$POD_NAME.service
+        sudo systemctl start pod-$POD_NAME.service
 
-    echo "Podman pod systemd service created and enabled: pod-$POD_NAME.service"
+        echo "Podman pod systemd service created and enabled: pod-$POD_NAME.service"
+        ;;
     *)
         echo "Unknown engine '$CONTAINER_ENGINE'. Cannot deploy." >&2
         exit 1

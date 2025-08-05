6a6dc451a2db6b7b0c63a2145e55a6af3225a8bc
pwildber <pwildber@iriusrisk.com>
2025-08-05 12:05:06 +0100

    DP-720: move all service files

--- scripts/one-click.sh
+++ scripts/one-click.sh
@@ -233,7 +233,7 @@ case "$CONTAINER_ENGINE" in
 
         # -- SYSTEMD autostart for the pod --
         sudo podman generate systemd --name $POD_NAME --files --restart-policy=always
-        sudo mv pod-$POD_NAME.service /etc/systemd/system/
+        sudo mv *.service /etc/systemd/system/
         sudo systemctl daemon-reload
         sudo systemctl enable pod-$POD_NAME.service
         sudo systemctl start pod-$POD_NAME.service
